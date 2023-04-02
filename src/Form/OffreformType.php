3cb22e6cb5b0888949ad9666820a332e24acbe36
fad-anes <anes.fad@esprit.tn>
2023-04-02 01:23:30 +0100

    controle saisie aj offre

--- src/Form/OffreformType.php
+++ src/Form/OffreformType.php
@@ -7,18 +7,31 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 class OffreformType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-            ->add('titre')
-            ->add('salaireh')
-            ->add('submit', SubmitType::class, [
-                'label' => 'ajouter'
-            ]);
-        ;
+        
+        ->add('titre', null, [
+            'label' => false,
+            'attr' => ['class' => 'i']
+        ])
+        ->add('salaireh', null, [
+            'label' => false,
+            'attr' => ['class' => 'i']
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => false,
+            'attr' => ['class' => 'i']
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'ajouter',
+            'attr' => ['class' => 'b'],
+        ]);
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
